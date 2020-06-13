8bd3c3803d4fe601f178ac1173130daabfbe2393
john-kevin <kevinsoriano2828@gmail.com>
2020-06-13 20:51:24 +0800

    Add thea mae soriano

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Thea'}
+    user = {'username': 'Thea Mae Soriano'}
     posts = [
         {
             'author': {'username': 'John'},
